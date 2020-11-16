6d2387d2837d6232fb5749c319b2e09430e63160
gilesadkins <gilesadkins@gmail.com>
2020-11-15 21:44:49 -0600

    made a list

--- script.js
+++ script.js
@@ -17,12 +17,11 @@ const populateTodos = () => {
 var i;
 
 for (i = 0; i < 25; i++) {
-
-const listOl = document.getElementById("todo-list")
-const element = document.createElement("li")
-const content = document.createTextNode(arrayOfTodos[i].title)
-listOl.appendChild(element)
-element.appendChild(content)
-console.log (listOl)
+  const listOl = document.getElementById("todo-list")
+  const element = document.createElement("li")
+  const content = document.createTextNode(arrayOfTodos[i].title)
+  listOl.appendChild(element)
+  element.appendChild(content)
+  console.log (listOl)
 }
-}
+}}
